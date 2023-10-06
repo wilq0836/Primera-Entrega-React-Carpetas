0161a81950530c9990c23ec8470c0d9dc461a67f
Wilmar Alexander Quintero Montoya <111884757+wilq0836@users.noreply.github.com>
2023-10-06 08:18:31 -0500

    Update Tarjeta.jsx

--- src/Componentes/Tarjeta.jsx
+++ src/Componentes/Tarjeta.jsx
@@ -1,47 +1,86 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 
-
+const carrito = [];
 function Targeta ({valorSeleccionadoLista}){
   
-
+  
   return ( 
-           
-           <div>
-                  {ImprimirTarjeta(valorSeleccionadoLista)}
-           </div>
-         );
+          imprimirTarjeta(valorSeleccionadoLista)
+          )
 
-}
-export default Targeta;
+ }
+
+
+  const imprimirTarjeta =(valor)=>{
+    
+   
+    return( 
+
+       
+    <div className="container1">{
+      
+      valor.map((item) => 
+
+      <div className="container" key = {item.id}>     
+        <div  className = "card m-3 p-3" >  
+              <img  className = "card-img-top" src={item.foto} alt={item.modelo}/>
+          
+            <div className="card-body" >     
+                <h5>ID: {item.id} </h5>               
+                <h5>Marca: {item.marca} </h5>
+                <h5>Modelo: {item.modelo }</h5>
+                <h5>Precio: {item.precio}</h5>
+                <h5>Especificación: {item.cantidad}</h5>                
+                 
+            </div>    
+              <button  onClick={()=>{
+              //let encontrarProducto = carrito.includes(item);
+              //const {id,marca,modelo,precio,especificacion,foto,cantidad} = item;
+              new Carrito(item);
+              carrito.push(item);
+              console.log(JSON.stringify(carrito)); 
+              Alerta();
+        
+                     }} key={item.id}   className="btn btn-secondary">Agregar al Carrito</button> 
+
+                <hr></hr>
+               <Link className="btn btn-primary" >Ver mas sobre este producto</Link>                    
+            </div>
+            
+            </div>  
+   
+
+    )}</div>
+   
+    )
+
+  }
+ 
+  export default Targeta;
+    
+
+  class Carrito {
+      constructor(id,marca, modelo, precio, especificacion, foto, cantidad) {
+          this.id = id;
+          this.marca = marca;
+          this.modelo = modelo;
+          this.precio = precio;
+          this.especificacion = especificacion;
+          this.foto = foto;
+          this.cantidad = cantidad;
+       }}
+        
   
 
+        const Alerta =()=>{
+          alert("Producto agregado al carrito");
+        }
 
-function ImprimirTarjeta(valor2){
 
-  const tarjeta =  valor2.map((item) => 
 
-   <div className="container" key = {item.id}>
-     <div  className= "row row-cols-1 row-cols-md-3 g-4" >
-       <div className = "card m-3 p-3" requiere  = "width: 20rem">       
-         <img  scr = { item.foto } className = "card-img-top"  alt= {item.modelo}  /> 
-           <div className="card-body">                    
-               <h5>Marca: {item.marca} </h5>
-               <h5>Modelo: {item.modelo }</h5>
-               <h5>Precio: {item.precio}</h5>
-               <h5>Especificación: {item.cantidad}</h5>                
-                
-           </div>    
-           <button className="btn btn-secondary">Agregar</button>             
-   </div>
-   </div>  
-   </div> )
-   
-     return(
-       
-       <div>
-         {tarjeta}
-       </div>
+
          
        
         )}
